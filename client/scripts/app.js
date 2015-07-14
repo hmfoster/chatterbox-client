b7e8783e4e34ca561eef8d9954e1c7522824a429
Hailey Foster <haileyhasmail@gmail.com>
2015-07-14 10:36:57 -0700

    Escape characters to prevent xss

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,6 +5,22 @@ var app = {
   timeStamp: new Date("October 13, 1976 11:13:00")
 };
 
+// Use the browser's built-in functionality to quickly and safely escape the
+// string
+var escapeHtml = function (str) {
+    var div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+};
+ 
+// UNSAFE with unsafe strings; only use on previously-escaped ones!
+var unescapeHtml = function (escapedStr) {
+    var div = document.createElement('div');
+    div.innerHTML = escapedStr;
+    var child = div.childNodes[0];
+    return child ? child.nodeValue : '';
+};
+
 // makes a new room?
 app.init = function(){};
 
@@ -27,7 +43,6 @@ app.send = function(data){
 
 app.fetch = function(){
 
-
   var readyState = $.get('https://api.parse.com/1/classes/chatterbox', function(data, status){
 
     app.messages = data.results;
@@ -45,32 +60,29 @@ app.addMessage = function(){
 //need to get dates to compare with each other
 
 // their date comes in this format: 2015-07-14T03:16:38.685Z
-//convert using new Date(app.messages[i].createdAt)
-//then just compare like we wanted: if (new Date(app.messages[i].createdAt) >app.timeStamp){}
 
   for (var i = 0; i < app.messages.length; i++) {
-    console.log(app.messages[i].createdAt > app.timeStamp);
-    console.log(app.messages[i].createdAt);
-    console.log(app.timeStamp);
-    if (app.messages[i].createdAt > app.timeStamp){
+    var dateString = new Date(app.messages[i].createdAt);
+    if (dateString > app.timeStamp){
+      
       var message = $('.allMessages').append('<div class = "message">');
-      var userName = $(('<span class = "userName">@'+ app.messages[i].username+'</span>'));
-      var dateCreated = $(('<span class = "dateCreated">'+ app.messages[i].createdAt+'</span>'));
-      var updatedAt = $(('<span class = "updatedAt">'+ app.messages[i].updatedAt+'</span>'));
-      var text = $(('<span class = "text">'+ app.messages[i].text+'</span>'));
+
+      var userName = '@' + escapeHtml( app.messages[i].username );
+      userName = $(('<span class = "userName">' + userName + '</span>'));
+      var dateCreated = escapeHtml( app.messages[i].createdAt );
+      dateCreated = $(('<span class = "dateCreated">'+ dateCreated +'</span>'));
+      var updatedAt = escapeHtml( app.messages[i].updatedAt );
+      updatedAt = $(('<span class = "updatedAt">' + updatedAt +'</span>'));
+      var text = escapeHtml( app.messages[i].text );
+      text = $(('<span class = "text">' + text + '</span>'));
       message.append(userName, userName, dateCreated,updatedAt,text);
     }
   }
 
   app.timeStamp = new Date();
-
-
 };
 
 app.addRoom = function(){};
-
-
-
 
 setInterval(function(){
   app.fetch();
