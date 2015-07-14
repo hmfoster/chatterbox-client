f7f7124d9c587486c6189c91a125b691bb46f72a
Hailey Foster <haileyhasmail@gmail.com>
2015-07-13 20:28:52 -0700

    Thoughts to only add new messages.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,7 +1,8 @@
 // YOUR CODE HERE:
 
 var app = {
-  server: 'https://api.parse.com/1/classes/chatterbox'
+  server: 'https://api.parse.com/1/classes/chatterbox',
+  timeStamp: new Date("October 13, 1976 11:13:00")
 };
 
 // makes a new room?
@@ -31,7 +32,7 @@ app.fetch = function(){
 
     app.messages = data.results;
 
-    console.log("data = ", data);
+    // console.log("data = ", data);
     return data.results;
   });
 };
@@ -40,25 +41,42 @@ app.clearMessages = function(){};
 
 app.addMessage = function(){
 
-  app.fetch();
-
   console.log("app.messages = ", app.messages);
+//need to get dates to compare with each other
+
+// their date comes in this format: 2015-07-14T03:16:38.685Z
+//convert using new Date(app.messages[i].createdAt)
+//then just compare like we wanted: if (new Date(app.messages[i].createdAt) >app.timeStamp){}
 
   for (var i = 0; i < app.messages.length; i++) {
-
-    var message = $('.allMessages').append('<div class = "message">');
-    var userName = $(('<span class = "userName">@'+ app.messages[i].username+'</span>'));
-    var dateCreated = $(('<span class = "dateCreated">'+ app.messages[i].dateCreated+'</span>'));
-    var updatedAt = $(('<span class = "updatedAt">'+ app.messages[i].updatedAt+'</span>'));
-    var text = $(('<span class = "text">'+ app.messages[i].text+'</span>'));
-    message.append(userName, userName, dateCreated,updatedAt,text);
-
+    console.log(app.messages[i].createdAt > app.timeStamp);
+    console.log(app.messages[i].createdAt);
+    console.log(app.timeStamp);
+    if (app.messages[i].createdAt > app.timeStamp){
+      var message = $('.allMessages').append('<div class = "message">');
+      var userName = $(('<span class = "userName">@'+ app.messages[i].username+'</span>'));
+      var dateCreated = $(('<span class = "dateCreated">'+ app.messages[i].createdAt+'</span>'));
+      var updatedAt = $(('<span class = "updatedAt">'+ app.messages[i].updatedAt+'</span>'));
+      var text = $(('<span class = "text">'+ app.messages[i].text+'</span>'));
+      message.append(userName, userName, dateCreated,updatedAt,text);
+    }
   }
+
+  app.timeStamp = new Date();
 
 
 };
 
 app.addRoom = function(){};
+
+
+
+
+setInterval(function(){
+  app.fetch();
+}, 1500);
+
+
 
 
 // var $newdiv1 = $( "<div id='object1'/>" )
